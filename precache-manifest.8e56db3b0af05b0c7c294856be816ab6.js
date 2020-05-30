6e06f5e33789027f28099b36b058d10509d6c986
Abhay Pratap Singh <abhaypratap3536@gmail.com>
2020-05-30 18:00:31 +0100

    Updates

---
+++ precache-manifest.8e56db3b0af05b0c7c294856be816ab6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9f8807bfd988bc9ef1bd74cca49778",
+    "url": "/personal-resume/index.html"
+  },
+  {
+    "revision": "6799c5af55005b0e6d5b",
+    "url": "/personal-resume/static/css/main.b99a0ccf.chunk.css"
+  },
+  {
+    "revision": "da294a7d83851e274bc9",
+    "url": "/personal-resume/static/js/2.a0d5c6b4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/personal-resume/static/js/2.a0d5c6b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6799c5af55005b0e6d5b",
+    "url": "/personal-resume/static/js/main.1f9b75f9.chunk.js"
+  },
+  {
+    "revision": "e109e6b39aa0cd269ef3",
+    "url": "/personal-resume/static/js/runtime-main.573a8857.js"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/personal-resume/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "9a77fbaa85fa42b73e3b96399daf49c5",
+    "url": "/personal-resume/static/media/Lato-Thin.9a77fbaa.ttf"
+  },
+  {
+    "revision": "22dd36f616c2ece8a4b1cc2217095003",
+    "url": "/personal-resume/static/media/SIMPLIFICA.22dd36f6.ttf"
+  },
+  {
+    "revision": "11463a7ed9658f9c31922a12918178aa",
+    "url": "/personal-resume/static/media/covid19.11463a7e.png"
+  },
+  {
+    "revision": "fe1e0f52de5bdb4ff308115b976f5495",
+    "url": "/personal-resume/static/media/githubLogo.fe1e0f52.png"
+  },
+  {
+    "revision": "7656b23cc92fe996843cabaa581ed703",
+    "url": "/personal-resume/static/media/movie.7656b23c.png"
+  },
+  {
+    "revision": "a95308e0acc82cd9a1088282636a8829",
+    "url": "/personal-resume/static/media/myblog.a95308e0.png"
+  },
+  {
+    "revision": "12d7acebbbc9b0375f5df3c2bac7d6f6",
+    "url": "/personal-resume/static/media/streetBG.12d7aceb.jpg"
+  }
+]);
